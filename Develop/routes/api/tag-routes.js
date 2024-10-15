1d594464fc51609599c6efdcc20a3071656d85b0
jcjohncarter3 <jcjohncarter3@gmail.com>
2024-10-14 20:34:01 -0500

    Worked on tag-routes id

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -20,6 +20,16 @@ router.get("/", (req, res) => {
 router.get("/:id", (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  Tag.findOne({
+    where: { id: req.params.id },
+    include: Product,
+  })
+    .then(function (data) {
+      res.json(data);
+    })
+    .catch(function (error) {
+      res.send(error);
+    });
 });
 
 router.post("/", (req, res) => {
